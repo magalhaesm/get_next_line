8b07843604297665b20b4099e67f831a963d1845
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-07-05 21:59:10 -0300

    refactor(bonus): add a buffer inside the struct

--- gnl/get_next_line_bonus.h
+++ gnl/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 16:34:00 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/07/05 10:28:36 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/07/05 21:45:13 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,17 @@
 # endif
 
 typedef struct s_chunk {
+	char			text[BUFFER_SIZE + 1];
 	int				size;
-	char			*text;
+	int				newline;
 	struct s_chunk	*next;
 }	t_chunk;
 
 char	*get_next_line(int fd);
+int		newline(t_chunk *storage);
 int		sum_chunks(t_chunk *storage);
-void	free_storage(t_chunk **storage);
+t_chunk	*new_chunk(void);
 t_chunk	*last_chunk(t_chunk *storage);
-t_chunk	*new_chunk(char *text);
+t_chunk	*writable_node(t_chunk **storage);
 
 #endif
